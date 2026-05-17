8977a6d3b565050928fcbd0a076c8a5dc62fa461
Sage KITSA <sagelusenge@gmail.com>
2026-05-17 10:24:02 +0200

    Update APK link for appreciations build

--- frontend/src/config/appLinks.js
+++ frontend/src/config/appLinks.js
@@ -1,1 +1,1 @@
-export const APP_DOWNLOAD_URL = 'https://expo.dev/artifacts/eas/dM1pfZwDBrDUEhTc2kvKy6.apk';
+export const APP_DOWNLOAD_URL = 'https://expo.dev/artifacts/eas/hMe3sX1Kc4ghqhXhBym9rG.apk';
